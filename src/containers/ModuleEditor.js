35424d4be59426bff48e02a048b2f03c6b34bd82
root <root@localdomain>
2018-08-16 21:38:16 -0400

    minor style change

--- src/containers/ModuleEditor.js
+++ src/containers/ModuleEditor.js
@@ -9,7 +9,7 @@ export default class ModuleEditor extends React.Component {
             return null;
         } else {
             return (
-                <div>
+                <div className="row">
                     <h2>{this.props.module.title}</h2>
                     <LessonTabs id={this.props.module.id}/>
                 </div>
